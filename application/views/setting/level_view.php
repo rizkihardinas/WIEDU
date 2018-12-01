cd07454be6a1c73966a0546c8bdaacd6fc949c6d
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2018-12-01 18:20:20 +0700

    Update level_view.php

--- application/views/setting/level_view.php
+++ application/views/setting/level_view.php
@@ -14,40 +14,43 @@
     </header>
     <div class="container-fluid relative animatedParent animateOnce">
         <br>
-            <div class="row">
-                <div class="col-md-8 col-xs-12">
-                    <div class="row">
-                        <div class="col-md-4 mb-2">
-                            <label for="level">Level</label>
-                            <input type="text" class="form-control" id="nama_level">
-                            <input type="hidden" class="form-control" id="kode_level">
-                        </div>
+        <div class="card no-b no-r col-md-4">
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="form-group col-12 m-0">
+                        <label for="level">Nama Level</label>
+                        <input type="text" class="form-control" id="nama_level">
+                                        <input type="hidden" class="form-control" id="kode_level">
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-8 col-xs-12">
-                    <center>
-                        <button class="btn btn-primary" id="btnSimpanLevel" status="simpan"><i class="icon icon-save"></i> Simpan</button>
-                    </center>
-                </div>
+            <hr>
+            <div class="card-body">
+                <button class="btn btn-primary" id="btnSimpanLevel" status="simpan"><i class="icon icon-save"></i> Simpan</button>
             </div>
+        </div>
+        
             <hr>
             <div class="row">
                 <div class="col-md-12">
-                    <table class="table table-bordered" id="table_level">
-                        <thead>
-                            <tr>
-                            	<th>No</th>
-                                <th>Level</th>
-                                <th>
-                                    <center>
-                                        <i class="icon icon-cogs"></i>
-                                    </center>
-                                </th>
-                            </tr>
-                        </thead>
-                    </table>
+                    <div class="panel panel-default">
+                        <div class="panel-heading"></div>
+                        <div class="panel-body">
+                            <table class="table table-bordered" id="table_level">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Level</th>
+                                        <th>
+                                            <center>
+                                                <i class="icon icon-cogs"></i>
+                                            </center>
+                                        </th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
     </div>
