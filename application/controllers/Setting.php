613faf75168eef5cf56bb8699839a2856ae46876
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2018-12-01 18:20:00 +0700

    Update Setting.php

--- application/controllers/Setting.php
+++ application/controllers/Setting.php
@@ -22,6 +22,28 @@ class Setting extends CI_Controller
 		$this->load->view('parts/sidebar');
 		$this->load->view('setting/setting_view');
 	}
+	function menu(){
+		$data['parent_menu'] = $this->menu_model->tampil_parent();
+		foreach ($data['parent_menu'] as $parent) {
+			$data['menu'] = $this->menu_model->tampil_menu($parent['kode_parent_menu']);
+		}
+		$this->load->view('parts/header');
+		$this->load->view('parts/menu',$data);
+		$this->load->view('parts/sidebar');
+		$this->load->view('setting/menu_view',$data);
+
+	}
+	function parent_menu(){
+		$data['parent_menu'] = $this->menu_model->tampil_parent();
+		foreach ($data['parent_menu'] as $parent) {
+			$data['menu'] = $this->menu_model->tampil_menu($parent['kode_parent_menu']);
+		}
+		$this->load->view('parts/header');
+		$this->load->view('parts/menu',$data);
+		$this->load->view('parts/sidebar');
+		$this->load->view('setting/parent_menu_view',$data);
+		
+	}
 	function get_kode(){
 		echo rand(00000,99999);
 	}
@@ -29,11 +51,21 @@ class Setting extends CI_Controller
 		header('Content-Type: application/json');
     	echo $this->setting_model->get_all_level();
 	}
+	function get_menu(){
+		header('Content-Type: application/json');
+    	echo $this->setting_model->get_all_menu_json();
+	}
+	function get_parent_menu(){
+		header('Content-Type: application/json');
+    	echo $this->setting_model->get_all_parent_menu_json();
+	}
 	function simpan_level(){
 		$nama_level =strtoupper($this->input->post('nama_level'));
 		$data = array(
+			
 			'kode_level' => $this->input->post('kode_level'),
-			'nama_level' => $nama_level
+			'nama_level' => $nama_level,
+
 			 );
 		if ($this->input->post('status') == 'simpan') {
 			$cek = count($this->setting_model->cek_level($nama_level));
@@ -49,6 +81,65 @@ class Setting extends CI_Controller
 			}
 		}else{
 			$query = $this->setting_model->ubah_level($data,$this->input->post('kode_level'));
+			if ($query) {
+				echo 2;
+			}else{
+				echo 0;
+			}
+		}
+		
+	}
+	function simpan_menu(){
+		$nama_menu =ucwords($this->input->post('nama_menu'));
+		$data = array(
+			'kode_menu' => $this->input->post('kode_menu'),
+			'kode_parent_menu' => $this->input->post('kode_parent_menu'),
+			'nama_menu' => $nama_menu,
+			'url' => $this->input->post('url')
+			 );
+		if ($this->input->post('status') == 'simpan') {
+			$cek = count($this->setting_model->cek_menu($nama_menu));
+			if ( $cek == 1) {
+				echo 3;
+			}else{
+				$query = $this->setting_model->simpan_menu($data);
+				if ($query) {
+					echo 1;
+				}else{
+					echo 0;
+				}	
+			}
+		}else{
+			$query = $this->setting_model->ubah_menu($data,$this->input->post('kode_menu'));
+			if ($query) {
+				echo 2;
+			}else{
+				echo 0;
+			}
+		}
+		
+	}
+	function simpan_parent_menu(){
+		$nama_parent_menu =ucwords($this->input->post('nama_parent_menu'));
+		$data = array(
+			'kode_parent_menu' => $this->input->post('kode_parent_menu'),
+			'icon_parent_menu' => $this->input->post('icon'),
+			'nama_parent_menu' => $nama_parent_menu
+			 );
+		if ($this->input->post('status') == 'simpan') {
+			$cek = count($this->setting_model->cek_parent_menu($nama_parent_menu));
+			if ( $cek == 1) {
+				echo 3;
+			}else{
+				$query = $this->setting_model->simpan_parent_menu($data);
+				if ($query) {
+					echo 1;
+				}else{
+					echo 0;
+				}	
+			}
+		}else{
+			$query = $this->setting_model->ubah_parent_menu($data,$this->input->post('kode_parent_menu'));
 			if ($query) {
 				echo 2;
 			}else{
@@ -83,6 +174,16 @@ class Setting extends CI_Controller
 		$query = $this->setting_model->cek_detail_level($kode);
 		echo json_encode($query);
 	}
+	function get_detail_menu(){
+		$kode = $this->input->post('kode_menu');
+		$query = $this->setting_model->cek_detail_menu($kode);
+		echo json_encode($query);
+	}
+	function get_detail_parent_menu(){
+		$kode = $this->input->post('kode_parent_menu');
+		$query = $this->setting_model->cek_detail_parent_menu($kode);
+		echo json_encode($query);
+	}
 	function delete_akses(){
 		$kode_akses = $this->input->post('kode_akses');
 		$query = $this->setting_model->hapus_akses($kode_akses);
@@ -95,6 +196,24 @@ class Setting extends CI_Controller
 	function delete_level(){
 		$kode_level = $this->input->post('kode_level');
 		$query = $this->setting_model->hapus_level($kode_level);
+		if ($query) {
+			echo 1;
+		}else{
+			echo 0;
+		}
+	}
+	function delete_menu(){
+		$kode_menu = $this->input->post('kode_menu');
+		$query = $this->setting_model->hapus_menu($kode_menu);
+		if ($query) {
+			echo 1;
+		}else{
+			echo 0;
+		}
+	}
+	function delete_parent_menu(){
+		$kode_parent_menu = $this->input->post('kode_parent_menu');
+		$query = $this->setting_model->hapus_parent_menu($kode_parent_menu);
 		if ($query) {
 			echo 1;
 		}else{
@@ -142,6 +261,7 @@ class Setting extends CI_Controller
 			echo 1;
 		}
 	}
+
 
 }
  ?>
