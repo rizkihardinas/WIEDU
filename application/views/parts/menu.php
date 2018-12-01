45eebeaa675689d59f6c21db79bf6c174f379e34
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2018-12-01 18:20:14 +0700

    Update menu.php

--- application/views/parts/menu.php
+++ application/views/parts/menu.php
@@ -9,7 +9,7 @@
                             $kode_parent_menu =$data['kode_parent_menu'];
                             $query = $this->db->query("SELECT * FROM tb_menu WHERE kode_parent_menu='".$kode_parent_menu."'");
                             foreach ($query->result_array() as $menu) {
-                                echo anchor($menu['link'],'<i class="icon icon-circle-o"></i>'.$menu['nama_menu']);
+                                echo anchor($menu['url'],'<i class="icon icon-circle-o"></i>'.$menu['nama_menu']);
                             }
                              ?>
                             
