459523c234a13979c58801388337d9b6e6718c30
katawilly <willysetiawan7@gmail.com>
2019-02-08 17:22:44 +0700

    Desain POS
    design pos

--- application/views/pos/pos_view.php
+++ application/views/pos/pos_view.php
@@ -12,234 +12,87 @@ $this->load->view('parts/sidebar');
     document.body.className += ' sidebar-collapse' + ' sidebar-offCanvas-lg';
 </script>
 
-    <header class="blue accent-2 relative nav-sticky" style="height: 150px;">
+    <header class="blue accent-2 relative nav-sticky" style="height: 110px;">
         <div class="container-fluid text-white">
-            <div class="row p-t-b-10 ">
-            </div>
         </div>
     </header>
     <div class="container relative animatedParent animateOnce pull-up-lg pull-up-md pull-up-sm">
       <div class="animated fadeInUpShort my-3 mb-5">
           
-          <div class="row no-gutters my-1 white">
-              <div class="col-md-8 b-r pl-3">
+          <div class="row gutters" style="
+                  height: 520px; ">
+              <div class="col-md-8 b-r pl-9">
                     <div class="row">
-                        <a href="" class="menu card shadow no-b p-2">
-                                <img class="card-img-top" src="assets/img/menu/M001.jpg" alt="">
-                                <div class="center">
-                                    <small>Es Kopi Susu</small>
-                                </div>
-                        </a>
-                        <a href="" class="menu card shadow no-b p-2">
-                                <img class="card-img-top" src="assets/img/menu/M002.jpg" alt="">
-                                <div class="center">
-                                    <small>Es Kopi Susu</small>
-                                </div>
-                        </a>
-                        <a href="" class="menu card shadow no-b p-2">
-                                <img class="card-img-top" src="assets/img/menu/M003.jpg" alt="">
-                                <div class="center">
-                                    <small>Es Kopi Susu</small>
-                                </div>
-                        </a>
-                    </div>
-              </div>
-              <div class="col-md-4">
-                <div class="col-md-12">
-                    <label>Customer</label>
-                  <div class="input-group mb-3">
-                      <input type="text" class="form-control">
-                      <div class="input-group-append">
-                          <button class="btn btn-primary"><i class="icon icon-plus"></i></button>
+                      <div class="barmenu">
+                      <div class="row">
+                        <div class="col-md-7 no-pad">&nbsp;</div>
+                        <div class="col-md-5"><input type="text" class="form-control" id="validationCustom01" placeholder="Cari Produk" required></div>
                       </div>
-                  </div>
-                </div>
-                <div class="col-md-12">
-                  <label>No Faktur</label>
-                  <input type="text" disabled class="form-control">
-                </div>
-                <div class="col-md-12">
-                  <label>Nama Barang</label>
-                  <select class="form-control">
-                    <option>Nama Barang</option>
-                  </select>
-                </div>
-                <hr>
-                <div class="col-md-12" style="overflow: scroll;height: 300px;">
-                  
-                </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="col-md-6">
-                    Total  barang
                     </div>
-                    <div class="col-md-5">
-                    Total 
                     </div>
-                  </div>
-                </div>
+                    <div class="row">
+                        <a href="" class="menu col-md-2 shadow no-b p-2">
+                          <span class="badge badge-primary right">Rp. 15.000</span>
+                          <img class="card-img-top" src="assets/img/menu/M001.jpg" alt="">
+                          <div class="nama">
+                              <small class="text-center">Es Kopi Susu BOTOL</small>
+                          </div>
+                        </a>
+                        <a href="" class="menu col-md-2 shadow no-b p-2">
+                          <span class="badge badge-primary right">Rp. 12.000</span>
+                          <img class="card-img-top" src="assets/img/menu/M002.jpg" alt="">
+                          <div class="nama">
+                              <small class="text-center">Es Kopi Susu</small>
+                          </div>
+                        </a>
+                        <a href="" class="menu col-md-2 shadow no-b p-2">
+                          <span class="badge badge-primary right">Rp. 10.000</span>
+                          <img class="card-img-top" src="assets/img/menu/M003.jpg" alt="">
+                          <div class="nama">
+                              <small class="text-center">French fries</small>
+                          </div>
+                        </a>
+                    </div>
               </div>
-          </div>
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="table-responsive white">
-                      <table class="table table-striped table-hover r-0">
-                          <thead>
+              <div class="col-md-4 det-pos">
+                <div class="row">
+                  <div class="col-md-12 no-pad" style="
+                  height: 230px; background: #fff;">
+                    
+                    <table class="table table-hover ">
+                      <tbody>
                           <tr class="no-b">
-                              <th>COIN NAME</th>
-                              <th>PRICE (USD)</th>
-                              <th>MARKET CAP</th>
-                              <th>CHANGE(24H)</th>
-                              <th>STATUS</th>
-                              <th>PURCHASE</th>
-                              <th></th>
-                          </tr>
-                          </thead>
-                          <tbody>
-                          <tr>
                               <td>
-                                  <div class="avatar avatar-md mr-3 mt-1 float-left">
-                                      <img height="48" width="48" class="text-green"
-                                           src="assets/fonts/icons/svg/cenz.svg"/>
-                                  </div>
-                                  <div>
-                                      <div>
-                                          <strong>Centurs</strong>
-                                      </div>
-                                      <small>CENZ</small>
-                                  </div>
+                                  <h6>ES KOPI SUSU BOTOL</h6>
+                                  <small class="text-muted">Coffee</small>
                               </td>
-                              <td>$560</td>
-                              <td>$1605</td>
-                              <td><i class="icon-angle-double-down text-red mr-2"></i>1,4,348</td>
-                              <td><span class="sparkline"
-                                        data-values="[2, 4, 3, 4, 5, 4, 5, 4, 3, 4, 5, 6, 7, 5, 4, 3, 5, 6]"
-                                        data-options='{
-                                          "type": "line",
-                                          "width": "100",
-                                          "height": "40",
-                                          "lineColor": "#7dc855",
-                                          "fillColor": "rgba(255, 255, 255, 0.1)",
-                                           "lineWidth": 2,
-                                          "spotColor": "#03a9f4",
-                                          "minSpotColor": "#03a9f4"
-                                    }'></span></td>
+                              <td>Rp. 15000</td>
                               <td>
-                                  <button type="button" class="btn btn-outline-primary btn-xs">BUY NOW</button>
-                              </td>
-                              <td>
-                                  <a href="panel-page-profile.html"><i class="icon-eye mr-3"></i></a>
+                                  <a class="btn-fab btn-fab-sm btn-primary text-white">
+                                      <i class="icon-trash"></i>
+                                  </a>
                               </td>
                           </tr>
-                          <tr>
+                          <tr class="no-b">
                               <td>
-                                  <div class="avatar avatar-md mr-3 mt-1 float-left">
-                                      <img height="48" width="48" class="text-green"
-                                           src="assets/fonts/icons/svg/eth.svg"/>
-                                  </div>
-                                  <div>
-                                      <div>
-                                          <strong>Ethereum</strong>
-                                      </div>
-                                      <small>ETH</small>
-                                  </div>
+                                  <h6>FRENCH FRIES</h6>
+                                  <small class="text-muted">Food</small>
                               </td>
-                              <td>$560</td>
-                              <td>$1605</td>
-                              <td><i class="icon-angle-double-down text-red mr-2"></i>1,4,348</td>
-                              <td><span class="sparkline"
-                                        data-values="[2, 4, 3, 4, 5, 4, 5, 4, 3, 4, 5, 6, 7, 5, 4, 3, 5, 6]"
-                                        data-options='{
-                                          "type": "line",
-                                          "width": "100",
-                                          "height": "40",
-                                          "lineColor": "#7dc855",
-                                          "fillColor": "rgba(255, 255, 255, 0.1)",
-                                           "lineWidth": 2,
-                                          "spotColor": "#03a9f4",
-                                          "minSpotColor": "#03a9f4"
-                                    }'></span></td>
+                              <td>Rp. 10000</td>
                               <td>
-                                  <button type="button" class="btn btn-outline-primary btn-xs">BUY NOW</button>
-                              </td>
-                              <td>
-                                  <a href="panel-page-profile.html"><i class="icon-eye mr-3"></i></a>
+                                  <a class="btn-fab btn-fab-sm btn-primary text-white">
+                                      <i class="icon-trash"></i>
+                                  </a>
                               </td>
                           </tr>
-                          <tr>
-                              <td>
-                                  <div class="avatar avatar-md mr-3 mt-1 float-left">
-                                      <img height="48" width="48" class="text-green"
-                                           src="assets/fonts/icons/svg/sbd.svg"/>
-                                  </div>
-                                  <div>
-                                      <div>
-                                          <strong>Ethereum</strong>
-                                      </div>
-                                      <small>SBD</small>
-                                  </div>
-                              </td>
-                              <td>$560</td>
-                              <td>$1605</td>
-                              <td><i class="icon-angle-double-down text-red mr-2"></i>1,4,348</td>
-                              <td><span class="sparkline"
-                                        data-values="[2, 4, 3, 34, 5, 4, 25, 4, 3, 4, 5, 22, 17, 5, 1, 3, 5, 6]"
-                                        data-options='{
-                                          "type": "line",
-                                          "width": "100",
-                                          "height": "40",
-                                          "lineColor": "#fcce54",
-                                          "fillColor": "rgba(255, 255, 255, 0.1)",
-                                           "lineWidth": 2,
-                                          "spotColor": "#03a9f4",
-                                          "minSpotColor": "#03a9f4"
-                                    }'></span></td>
-                              <td>
-                                  <button type="button" class="btn btn-outline-primary btn-xs">BUY NOW</button>
-                              </td>
-                              <td>
-                                  <a href="panel-page-profile.html"><i class="icon-eye mr-3"></i></a>
-                              </td>
-                          </tr>
-                          <tr>
-                              <td>
-                                  <div class="avatar avatar-md mr-3 mt-1 float-left">
-                                      <img height="48" width="48" class="text-green"
-                                           src="assets/fonts/icons/svg/ada.svg"/>
-                                  </div>
-                                  <div>
-                                      <div>
-                                          <strong>Ethereum</strong>
-                                      </div>
-                                      <small>ETH</small>
-                                  </div>
-                              </td>
-                              <td>$560</td>
-                              <td>$1605</td>
-                              <td><i class="icon-angle-double-down text-red mr-2"></i>1,4,348</td>
-                              <td><span class="sparkline"
-                                        data-values="[2, 4, 3, 4, 5, 4, 5, 4, 3, 4, 5, 6, 7, 5, 4, 3, 5, 6]"
-                                        data-options='{
-                                          "type": "line",
-                                          "width": "100",
-                                          "height": "40",
-                                          "lineColor": "#ed5564",
-                                          "fillColor": "rgba(255, 255, 255, 0.1)",
-                                           "lineWidth": 2,
-                                          "spotColor": "#03a9f4",
-                                          "minSpotColor": "#03a9f4"
-                                    }'></span></td>
-                              <td>
-                                  <button type="button" class="btn btn-outline-primary btn-xs">BUY NOW</button>
-                              </td>
-                              <td>
-                                  <a href="panel-page-profile.html"><i class="icon-eye mr-3"></i></a>
-                              </td>
-                          </tr>
-                          </tbody>
+                        </tbody>
                       </table>
                   </div>
+                  <hr>
+
+                                    <button type="button" class="btn btn-primary btn-lg btn-block"><b>BAYAR (Rp. 30.000)</b></button>
               </div>
+            </div>
           </div>
       </div>
     </div>
